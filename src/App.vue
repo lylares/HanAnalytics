9c50f734ee932dd1d89d53aa6e67d4ebb2ae0e53
lylares <lylares@users.noreply.github.com>
2025-02-23 23:24:59 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,16 @@
     <header>
       <div class="main">
         <div class="logo">
-          <img src="./assets/favicon.ico">
-          <span>Han Analytics</span>
+          <span>Analytics</span>
         </div>
-        <h2>简单优雅的Web分析</h2>
+        <h2></h2>
       </div>
     </header>
     <main>
       <header>
         <Alert>
           <AlertDescription>
-            <p>· Han Analytics 是一个简单的网络分析跟踪器和仪表板，托管在被称为赛博菩萨的 Cloudflare 上,无成本稳定运行,每天可达10万次免费统计。</p>
-            <p>· 域名、服务器、数据库 通通都不用! 托管在 Cloudflare Pages 上即可快速部署网站分析仪表板。</p>
-            <p style="font-weight: bold;">· 开源地址: <a class="git-link" href="https://www.lylares.com"
-                target="_blank">Han-Analytics</a>
+            <p style="font-weight: bold;">
             </p>
           </AlertDescription>
         </Alert>
